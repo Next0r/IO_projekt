9321f9e76d960a3939e3cd7a301613a6f8d12cb9
kaes1 <kaesonline@gmail.com>
2019-02-02 22:48:23 +0100

    02.02.19 Posprzątane ProductSales.

--- Mojeauto/src/java/pl/polsl/io/controller/servlet/ProductSales.java
+++ Mojeauto/src/java/pl/polsl/io/controller/servlet/ProductSales.java
@@ -60,114 +60,34 @@ public class ProductSales extends HttpServlet {
         
         String stage = request.getParameter("stage");
         if (stage == null) {
-            // fetch package offer
-            ArrayList<pl.polsl.io.model.Package> packageOffer;
-            try {
-                packageOffer = new ArrayList<>(databaseService.getPackages(emf));
-                // cheapest packages first
-                Collections.sort(packageOffer, (pl.polsl.io.model.Package lhs, pl.polsl.io.model.Package rhs) -> lhs.getPrice() > rhs.getPrice() ? 1 : -1);
-                request.getSession().setAttribute("packages", packageOffer);
-            } catch (Exception e) {
-                // db exception
-            }
-
-            // fetch sevices offer
-            ArrayList<SingleService> serviceOffer;
-            try {
-                serviceOffer = new ArrayList<>(databaseService.getSingleServices(emf));
-                // cheapest packages first
-                Collections.sort(serviceOffer, (SingleService lhs, SingleService rhs) -> lhs.getPrice() > rhs.getPrice() ? 1 : -1);
-                request.getSession().setAttribute("services", serviceOffer);
-            } catch (Exception e) {
-                // db exception
-            }
+            displayOffer(request);
             request.getRequestDispatcher("/OurProductsPage.jsp").forward(request, response);  
         } else if (stage.equals("packageSelected")) {
-            // sell package if selected
+            // A package was selected for buying.
             String selPackageId = request.getParameter("packageSelected");
-            if (selPackageId != null) {
-                // fetch client entity and his cars
-                UserAccount acc;
-                Client cln;
-                ArrayList<ClientCar> clientCars;
-                try {
-                    acc = databaseService.getUserAccountEntity((String) request.getSession().getAttribute("currentUser"), "", emf);
-                    cln = databaseService.getClientEntityByAccount(acc, emf);
-                    clientCars = new ArrayList<>(databaseService.getClientCarsByClient(cln, emf));
-                } catch (Exception e) {
-                    // db exception
-                    inputDataService.generateErrorResultMessage();
-                    inputDataService.setResultMessageAttribute(null, request);
-                    request.getRequestDispatcher("/OurProductsPage.jsp").forward(request, response);
-                    return;
-                }
-                // check if client has set up his personal data
-                if (!inputDataService.isClientDataComplete(cln)) {
-                    inputDataService.setResultMessageAttribute(null, request);
-                    request.getRequestDispatcher("/OurProductsPage.jsp").forward(request, response);
-                    return;
-                }
-                // check if client has cars
-                if (clientCars.isEmpty()) {
-                    inputDataService.setResultMessageAttribute("There are no vehicles related to your account.", request);
-                    request.getRequestDispatcher("/OurProductsPage.jsp").forward(request, response);
-                    return;
-                }
-                // request car selection display
-                request.getSession().setAttribute("carSelection", true);
-                request.getSession().setAttribute("clientCars", clientCars);
-                request.getRequestDispatcher("/OurProductsPage.jsp").forward(request, response);
-
-                // selPackageId is allways package id (as generated number)
-                Integer selectedPackageId = Integer.valueOf(selPackageId);
-                request.getSession().setAttribute("selectedProductId", selectedPackageId);
+            if (selPackageId != null && isClientDataCorrect(request)){
+                displayCarSelection(request);
+                // selPackageId is allways product id (as generated number)
+                Integer selectedServiceId = Integer.valueOf(selPackageId);
+                request.getSession().setAttribute("selectedProductId", selectedServiceId);  
             }
+            request.getRequestDispatcher("/OurProductsPage.jsp").forward(request, response);
         } else if (stage.equals("serviceSelected")) {
-            // sell service if selected
+            // A service was selected for buying.
             String selServiceId = request.getParameter("serviceSelected");
-            if (selServiceId != null) {
-                // fetch client entity and his cars
-                UserAccount acc;
-                Client cln;
-                ArrayList<ClientCar> clientCars;
-                try {
-                    acc = databaseService.getUserAccountEntity((String) request.getSession().getAttribute("currentUser"), "", emf);
-                    cln = databaseService.getClientEntityByAccount(acc, emf);
-                    clientCars = new ArrayList<>(databaseService.getClientCarsByClient(cln, emf));
-                } catch (Exception e) {
-                    // db exception
-                    inputDataService.generateErrorResultMessage();
-                    inputDataService.setResultMessageAttribute(null, request);
-                    request.getRequestDispatcher("/OurProductsPage.jsp").forward(request, response);
-                    return;
-                }
-                // check if client has set up his personal data
-                if (!inputDataService.isClientDataComplete(cln)) {
-                    inputDataService.setResultMessageAttribute(null, request);
-                    request.getRequestDispatcher("/OurProductsPage.jsp").forward(request, response);
-                    return;
-                }
-                // check if client has cars
-                if (clientCars.isEmpty()) {
-                    inputDataService.setResultMessageAttribute("There are no vehicles related to your account.", request);
-                    request.getRequestDispatcher("/OurProductsPage.jsp").forward(request, response);
-                    return;
-                }
-                // request car selection display
-                request.getSession().setAttribute("carSelection", true);
-                request.getSession().setAttribute("clientCars", clientCars);
-                request.getRequestDispatcher("/OurProductsPage.jsp").forward(request, response);
-
-                // selPackageId is allways package id (as generated number)
+            if (selServiceId != null && isClientDataCorrect(request)) {
+                displayCarSelection(request);
+                // selServiceId is allways product id (as generated number)
                 Integer selectedServiceId = Integer.valueOf(selServiceId);
-                request.getSession().setAttribute("selectedProductId", selectedServiceId);
+                request.getSession().setAttribute("selectedProductId", selectedServiceId);  
             }
+            request.getRequestDispatcher("/OurProductsPage.jsp").forward(request, response);
         } else if (stage.equals("carsSelected")) {
             // check if user already selected cars
             String carsSelected = request.getParameter("carsSelected");
             if (carsSelected != null) {
                 // user met all requirements and selected cars
-                // package id in selectedPackageId session attribute
+                // product id in selectedProductId session attribute
                 Integer selectedProductId = (Integer) request.getSession().getAttribute("selectedProductId");
                 // cars displayed in clientCars session attribute
                 ArrayList<ClientCar> clientCars = (ArrayList<ClientCar>) request.getSession().getAttribute("clientCars");
@@ -264,6 +184,75 @@ public class ProductSales extends HttpServlet {
                 request.getRequestDispatcher("/OurProductsPage.jsp").forward(request, response);
             }
         } 
+    }
+    
+    private void displayOffer(HttpServletRequest request) {
+            // fetch package offer
+            ArrayList<pl.polsl.io.model.Package> packageOffer;
+            try {
+                packageOffer = new ArrayList<>(databaseService.getPackages(emf));
+                // cheapest packages first
+                Collections.sort(packageOffer, (pl.polsl.io.model.Package lhs, pl.polsl.io.model.Package rhs) -> lhs.getPrice() > rhs.getPrice() ? 1 : -1);
+                request.getSession().setAttribute("packages", packageOffer);
+            } catch (Exception e) {
+                // db exception
+            }
+            // fetch sevices offer
+            ArrayList<SingleService> serviceOffer;
+            try {
+                serviceOffer = new ArrayList<>(databaseService.getSingleServices(emf));
+                // cheapest packages first
+                Collections.sort(serviceOffer, (SingleService lhs, SingleService rhs) -> lhs.getPrice() > rhs.getPrice() ? 1 : -1);
+                request.getSession().setAttribute("services", serviceOffer);
+            } catch (Exception e) {
+                // db exception
+            }
+    }
+    
+    private boolean isClientDataCorrect(HttpServletRequest request) {
+        UserAccount acc;
+        Client cln;
+        ArrayList<ClientCar> clientCars;
+        try {
+            acc = databaseService.getUserAccountEntity((String) request.getSession().getAttribute("currentUser"), "", emf);
+            cln = databaseService.getClientEntityByAccount(acc, emf);
+            clientCars = new ArrayList<>(databaseService.getClientCarsByClient(cln, emf));
+        } catch (Exception e) {
+            // db exception
+            inputDataService.generateErrorResultMessage();
+            inputDataService.setResultMessageAttribute(null, request);
+            return false;
+        }
+        // check if client has set up his personal data
+        if (!inputDataService.isClientDataComplete(cln)) {
+            inputDataService.setResultMessageAttribute(null, request);
+            return false;
+        }
+        // check if client has cars
+        if (clientCars.isEmpty()) {
+            inputDataService.setResultMessageAttribute("There are no vehicles related to your account.", request);
+            return false;
+        }
+        return true;
+    }
+    
+    private void displayCarSelection(HttpServletRequest request) {
+        UserAccount acc;
+        Client cln;
+        ArrayList<ClientCar> clientCars;
+        try {
+            acc = databaseService.getUserAccountEntity((String) request.getSession().getAttribute("currentUser"), "", emf);
+            cln = databaseService.getClientEntityByAccount(acc, emf);
+            clientCars = new ArrayList<>(databaseService.getClientCarsByClient(cln, emf));
+        } catch (Exception e) {
+            // db exception
+            inputDataService.generateErrorResultMessage();
+            inputDataService.setResultMessageAttribute(null, request);
+            return;
+        }
+        // request car selection display
+        request.getSession().setAttribute("carSelection", true);
+        request.getSession().setAttribute("clientCars", clientCars);
     }
 
     /**
